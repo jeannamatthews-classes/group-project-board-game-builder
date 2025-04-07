50c0aa50e24098e3117ea2ada56f8adc84832610
jacksonfixsen <jacksonfixsen@gmail.com>
2025-04-06 20:54:42 -0400

    Updated TileType save code
    Submitted wrong version

--- Classes/TileType.js
+++ Classes/TileType.js
@@ -9,38 +9,12 @@ class TileType {
         this.scripts = scripts;
         this.typeName = name;
         
-     // Setter
-        setPublicVar(name, value) {
-        this.publicVars.set(name, value);
-    }
-
-    // Getter
-    getPublicVar(name) {
-        return this.publicVars.get(name);
-    }
-
-    // Method to generate a saveable code for tileType
     saveCode() {
         return {
             typeID: this.typeID,
             typeName: this.typeName,
-            scripts: this.scripts.map(script => ({
-                trigger: script.trigger,
-                run: script.run.toString(), 
-            })),
-            publicVars: this.publicVars.map(pv => ({ name: pv.name, value: pv.value }))
+            publicVars: this.publicVars,
+            scripts: this.scripts.map(s => s.saveCode ? s.saveCode() : null)
         };
-    }
-
-    // Method to load tileType from a save
-    static loadCode(saveCode) {
-        const tileType = new TileType(saveCode.typeName, saveCode.scripts.map(script => ({trigger: script.trigger, run: new Function('return ' + script.run)(), })), saveCode.typeID);
-
-        tileType.publicVars = saveCode.publicVars.map(pv => ({ name: pv.name, value: pv.value }));
-
-        return tileType;
-    
-
-        
     }
 }
